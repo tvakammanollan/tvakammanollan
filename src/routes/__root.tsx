53b162e90e9687cf91308a05ef2214f910f0f15a
niklas63 <niklas.pellkvist@gmail.com>
2026-05-10 22:19:37 +0000

    Changes

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -91,6 +91,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { property: "og:type", content: "website" },
     ],
     links: [
+      { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
